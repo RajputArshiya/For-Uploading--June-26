d146b50bd0f72a836752f9472e637e1ae4ea6a96
SourabhEduco <141809175+SourabhEduco@users.noreply.github.com>
2024-06-26 16:02:36 +0530

    Add files via upload

---
+++ 26-06/O0011Disc Ram/content_files/app.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+$(document).ready(function(){
+						//////////use "spaceclick" variable////////////////////////////////////////////////////////  
+					 window.abc=0;
+						   
+				      Local1=function(){
+						console.log("Please add my shange")
+					  }	
+			     	  Local2=function(){}
+			    	  Local3=function(){}
+				      Local4=function(){}
+				      Local5=function(){}
+							  	  
+						
+						  
+						   });
